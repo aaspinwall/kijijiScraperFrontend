559c7abda444d6fd12e015f4b0146a5c7b1888fe
Alejandro Aspinwall <aaspinwall@gmail.com>
2020-07-21 11:13:57 -0400

    initial lightweight server instance

--- src/setupProxy.js
+++
@@ -1,8 +0,0 @@
-const { createProxyMiddleware } = require("http-proxy-middleware");
-module.exports = function (app) {
-  app.use(
-    createProxyMiddleware("/search", {
-      target: "http://localhost:5000/search",
-    })
-  );
-};
